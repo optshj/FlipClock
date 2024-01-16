b77165346bbff27fe5a1fbb0db47cf3364d2cceb
optshj <optshj@gmail.com>
2024-01-16 13:47:35 +0000

    [Init Project]

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FlipClock from './components/js/FlipClock';
+
+function App() {
+  return (
+    <div className="App">
+		<FlipClock></FlipClock>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/js/FlipClock.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function FlipClock() {
+	
+	
+	return(
+		<>
+		
+		</>
+	)
+}
+
+export default FlipClock;

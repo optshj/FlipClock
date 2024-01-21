6bfb2308d6b8946f04bef50d53d2a58d4b007e7e
optshj <optshj@gmail.com>
2024-01-21 18:26:02 +0900

    [Feat] FlipClock삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import Clock from './components/js/Clock';
 
 function App() {
@@ -8,16 +7,5 @@ function App() {
         <Clock></Clock>
       </div>
     );
-=======
-import FlipClock from './components/js/FlipClock';
-
-function App() {
-  return (
-    <div className="App">
-		  <FlipClock></FlipClock>
-    </div>
-  );
->>>>>>> f4f154c7ab75547dda2e621008939a12fc4da1f8
 }
-
 export default App;

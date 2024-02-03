550bc23e3636c0ac3a52bdc14675ec5bb9fd5dcc
optshj <optshj@gmail.com>
2024-02-03 05:14:52 +0000

    [Feat] 업데이트

---
+++ src/components/js/Card.tsx
@@ -0,0 +1,24 @@
+import React,{useState,useEffect} from 'react';
+import styled from 'styled-components';
+
+const Template = styled.div`
+`
+
+const Upper = styled.div`
+	transform-origin: 50% 100%;
+`
+
+const Lower = styled.div`
+	transform-origin: 50% 0%;
+`
+
+function Card(){
+	
+	
+	return(
+		<>
+		
+		</>
+	)
+}
+export default Card;
--- src/components/js/FlipClock.tsx
+++ src/components/js/FlipClock.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import styled from 'styled-components';
 
 
 function FlipClock() {
+	const [time,setTime] = useState(0);
 	
+	useEffect(()=> {
+		setInterval(setTime(new Date()),1000); // 1초마다 시간 최신화
+	},[]);
 	
 	return(
 		<>
-		
+
 		</>
 	)
 }
